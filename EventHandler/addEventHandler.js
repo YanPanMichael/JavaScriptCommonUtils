dfbf6c59cf0843a67a14eed9af5a56ed4b1991f3
YanPanMichael <yanpancsu@icloud.com>
2018-08-06 23:09:58 +0800

    save

--- EventHandler/addEventHandler.js
+++ EventHandler/addEventHandler.js
@@ -4,7 +4,7 @@ type: resize, scroll (event type)
 callback: the function reference 
 */
 
-var addEveHandler = function(object, type, callback) {
+const addEveHandler = function(object, type, callback) {
     if (object == null || typeof(object) == 'undefined') return;
     if (object.addEventListener) {
         object.addEventListener(type, callback, false);
@@ -19,3 +19,5 @@ var addEveHandler = function(object, type, callback) {
 addEveHandler(window, "resize", function(event) {
   console.log('resized');
 });
+
+export default addEveHandler;
