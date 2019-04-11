a57b61391385a0877eeaafd4060b81a2520c9ac9
YanPanMichael <yanpancsu@icloud.com>
2019-04-12 00:01:09 +0800

    recursion timeout

--- Recursion/countDown.js
+++ Recursion/countDown.js
@@ -3,5 +3,19 @@ const countDown = (value, fn) => {
   value > 0 ? countDown(value -1, fn) : value;
 }
 
-countDown(10, console.log);
-countDown(10, input => console.log(input));
+// countDown(10, console.log);
+// countDown(10, input => console.log(input));
+
+let timeoutRef = null;
+const countDownDelay = (value, fn, delay) => {
+  fn(value);
+  if(timeoutRef) {
+    clearTimeout(timeoutRef);
+    timeoutRef = null;
+  };
+  if(value > 0) {
+    timeoutRef = setTimeout(() => countDownDelay(value-1, fn, delay), delay);
+  }
+}
+
+countDownDelay(10, console.log, 1000);
