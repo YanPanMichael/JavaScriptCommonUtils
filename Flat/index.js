d633e9cb2278cce4a663831420fc8522173c3521
YanPanMichael <yanpancsu@icloud.com>
2019-10-18 01:16:54 +0800

    flat => reduce and concat

--- Flat/index.js
+++ Flat/index.js
@@ -5,3 +5,13 @@ arr1.flat();
 var arr2 = [1, 2, [3, 4, [5, 6]]];
 arr2.flat();
 // [1, 2, 3, 4, [5, 6]]
+
+//reduce and concat
+var arr = [1,2,[3,4]]
+arr.flat();
+//is equals to
+arr.reduce((acc, val) => {
+  return acc.concat(val)
+}, [])
+//or with decomposition syntax
+const flatted = arr => [].concat(...arr)
