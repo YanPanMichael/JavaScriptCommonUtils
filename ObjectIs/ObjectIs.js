f555f374d2a8bf4e07839918dcd737c1da7a2e5f
YanPanMichael <yanpancsu@icloud.com>
2019-10-24 00:03:33 +0800

    Object.is polyfill

--- ObjectIs/ObjectIs.js
+++ ObjectIs/ObjectIs.js
@@ -1,5 +1,14 @@
 // Object.is polyfill
 
-(function(){
-  
-})()
+(function() {
+  if (!Object.is) {
+    Object.is = function(x, y) {
+      // SameValue algorithm
+      if (x === y) {
+        return x !== 0 || 1 / x === 1 / y;
+      } else {
+        return x !== x && y !== y;
+      }
+    };
+  }
+})();
